10e58fe4f3b5f42c2c62d645beb8abec94885c41
dungnt12 <nihilism.core@gmail.com>
2018-06-25 18:23:52 +0700

    gioi thieu ban than

--- 1.js
+++ 1.js
@@ -1,4 +1,7 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(document).ready(function() {
+	$('.chuot').click(function(event) {
+		$('html, body').animate({
+			scrollTop: $('.mt-5').offset().top
+		})
+	});
+});
